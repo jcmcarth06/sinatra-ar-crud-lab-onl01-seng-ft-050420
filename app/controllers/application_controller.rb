b81793e4cd05b93827045a4d27fa5c1a805ee725
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-19 20:11:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,44 +2,49 @@
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  
   get '/' do
     redirect to '/articles'
   end
-
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
   get '/articles/new' do
     erb :new
   end
-
+  
   get '/articles/:id' do
     @article = Article.find_by(id: params[:id])
     erb :show
   end
-
+  
   post '/articles' do
     Article.create(title: params[:title], content: params[:content])
-     redirect to '/articles/' + "#{article[:id]}"
+    redirect to '/articles/' + "#{Article.last.id}"
   end
-
+  
   get '/articles/:id/edit' do
-    Article.find(params[:id])
+    @article = Article.find(params[:id])
     erb :edit
   end
-
-  patch 'articles/:id' do
-    @article = Article.find(params[:id])
-    Article.update(title: params[:title], content: params[:content])
-    retirect to 'articles/' + "#{article[:id]}"
+  
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    article.update(title: params[:title], content: params[:content])
+    redirect to '/articles/' + "#{article[:id]}"
   end
-
+  
   delete '/articles/:id' do
     Article.destroy(params[:id])
     redirect to '/articles'
   end
-
+  
 end
